50dc90fdbbed06f2c956622f606b77e3d22e20e8
Babita00 <babitaasth07@gmail.com>
2024-07-20 08:51:53 +0545

    day 1 name update

---
+++ 1.DAY 1 Variables_and_Data_types.js
@@ -0,0 +1,50 @@
+var n = 10;
+console.log("Declared value:", n);
+
+let str = "Babita";
+console.log("The string :", str);
+
+const b = true;
+console.log("Boolean value is:", b);
+let person = {
+  firstName: "John",
+  lastName: "Doe",
+  age: 30,
+};
+console.log(person);
+console.log(typeof n, typeof str, typeof b);
+
+let v = 10; // Initial value
+console.log("Initial value:", v);
+
+v = 99; // Reassign a new value
+console.log("New value:", v);
+
+// Variable Types Console Log
+
+// Declaring variables of different data types
+let number = 42; // Number
+let string = "Hello, World!"; // String
+let boolean = true; // Boolean
+let array = [1, 2, 3]; // Array
+let object = {
+  // Object
+  name: "Alice",
+  age: 30,
+};
+let undefinedVariable; // Undefined
+let nullVariable = null; // Null
+
+// Logging the value and type of each variable
+console.log("number:", number, ", type:", typeof number);
+console.log("string:", string, ", type:", typeof string);
+console.log("boolean:", boolean, ", type:", typeof boolean);
+console.log("array:", array, ", type:", typeof array);
+console.log("object:", object, ", type:", typeof object);
+console.log(
+  "undefinedVariable:",
+  undefinedVariable,
+  ", type:",
+  typeof undefinedVariable
+);
+console.log("nullVariable:", nullVariable, ", type:", typeof nullVariable);
